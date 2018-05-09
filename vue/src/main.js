343b586291579877efcd1514949f8b67d23dc0a0
grassclipper <grassclipper@users.noreply.github.com>
2018-05-08 23:36:46 -0500

    installed firebase in app

--- vue/src/main.js
+++ vue/src/main.js
@@ -1,7 +1,17 @@
 import Vue from 'vue'
 import App from './App.vue'
+import firebase from 'firebase'
 
 Vue.config.productionTip = false
+
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyDQOhAqHtOOjWetSbzfvdlpXB_RMtfb_NU",
+  authDomain: "firstinspiresiowa2018.firebaseapp.com",
+//  databaseURL: "https://<DATABASE_NAME>.firebaseio.com",
+//  storageBucket: "<BUCKET>.appspot.com",
+};
+firebase.initializeApp(config);
 
 new Vue({
   render: h => h(App)
